6dbc08871e01e25f9c294b9a4221b9d36a87601a
Yoon ho <dbsgh431@jbnu.ac.kr>
2022-12-23 15:21:18 +0000

    deeplabv3+_r101_40k (base) 실험

--- CV03/configs/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_40k_cityscapes.py
+++ CV03/configs/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_40k_cityscapes.py
@@ -13,26 +13,27 @@ model = dict(
     decode_head=dict(num_classes=11), 
     auxiliary_head=dict(num_classes=11))
 
+
+
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=10)
-checkpoint_config = dict(interval=20, max_keep_ckpts=5)
-evaluation = dict(interval=1, metric='mIoU', pre_eval=True)
-
+runner = dict(type='IterBasedRunner', max_iters=40000)
+checkpoint_config = dict(by_epoch=False, interval=4000)
+evaluation = dict(interval=4000, metric='mIoU', pre_eval=True)
 
 
 
 wandb.login()
 # yapf:disable
 log_config = dict(
-    interval=50,
+    interval=100,
     hooks=[
-        dict(type='TextLoggerHook', by_epoch=True),
-        dict(type='WandbLoggerHook',interval=50,
+        dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='WandbLoggerHook',interval=100,
             init_kwargs=dict(
                 project='Segmentation_project',
                 entity = 'aitech4_cv3',
