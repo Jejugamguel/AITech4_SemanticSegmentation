7aa66c6cf4790652f4e3f8d6198bcac438e581e7
Yoon ho <dbsgh431@jbnu.ac.kr>
2022-12-23 16:17:33 +0000

    epochbased runner로 변경

--- CV03/configs/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_40k_cityscapes.py
+++ CV03/configs/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_40k_cityscapes.py
@@ -15,16 +15,24 @@ model = dict(
 
 
 
+# # optimizer
+# optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+# optimizer_config = dict()
+# # learning policy
+# lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+# # runtime settings
+# runner = dict(type='IterBasedRunner', max_iters=40000)
+# checkpoint_config = dict(by_epoch=False, interval=4000)
+# evaluation = dict(interval=4000, metric='mIoU', pre_eval=True)
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
-runner = dict(type='IterBasedRunner', max_iters=40000)
-checkpoint_config = dict(by_epoch=False, interval=4000)
-evaluation = dict(interval=4000, metric='mIoU', pre_eval=True)
-
+runner = dict(type='EpochBasedRunner', max_epochs=60)
+checkpoint_config = dict(interval=5, max_keep_ckpts=5)
+evaluation = dict(interval=1, metric='mIoU', pre_eval=True)
 
 
 wandb.login()
@@ -32,12 +40,12 @@ wandb.login()
 log_config = dict(
     interval=100,
     hooks=[
-        dict(type='TextLoggerHook', by_epoch=False),
+        dict(type='TextLoggerHook', by_epoch=True),
         dict(type='WandbLoggerHook',interval=100,
             init_kwargs=dict(
                 project='Segmentation_project',
                 entity = 'aitech4_cv3',
-                name = "wandb_test"),)
+                name = "DeepLabv3+_resnet101_60e"),)
         # log_checkpoint=True,
         # log_checkpoint_metadata=True,
         # dict(type='TensorboardLoggerHook')
