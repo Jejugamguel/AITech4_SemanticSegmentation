c1bab7b75d90fc206a8ecbb6f2ca0961e7da68de
Yoon ho <dbsgh431@jbnu.ac.kr>
2022-12-24 08:14:05 +0000

    wandb val 출력 오류 수정

--- CV03/configs/fcn_d6_r50-d16_512x1024_40k_cityscapes.py
+++ CV03/configs/fcn_d6_r50-d16_512x1024_40k_cityscapes.py
@@ -1,8 +1,45 @@
+import wandb
 _base_ = [
     '../../mmsegmentation/configs/_base_/models/fcn_r50-d8.py', '../_base_/custom.py',
-    '../../mmsegmentation/configs/_base_/default_runtime.py', '../../mmsegmentation/configs/_base_/schedules/schedule_40k.py'
+    '../../mmsegmentation/configs/_base_/default_runtime.py'
 ]
 model = dict(
     backbone=dict(dilations=(1, 1, 1, 2), strides=(1, 2, 2, 1)),
     decode_head=dict(dilation=6,num_classes=11),
     auxiliary_head=dict(dilation=6, num_classes=11))
+
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
+# runtime settings
+runner = dict(type='EpochBasedRunner', max_epochs=10)
+checkpoint_config = dict(interval=5, max_keep_ckpts=5)
+evaluation = dict(interval=1, metric='mIoU', pre_eval=True)
+
+
+wandb.login()
+# yapf:disable
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=True),
+        dict(type='WandbLoggerHook',interval=100,
+            init_kwargs=dict(
+                project='Segmentation_project',
+                entity = 'aitech4_cv3',
+                name = "test_wandb"),)
+        # log_checkpoint=True,
+        # log_checkpoint_metadata=True,
+        # dict(type='TensorboardLoggerHook')
+        # dict(type='PaviLoggerHook') # for internal services
+    ])
+
+
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+cudnn_benchmark = True
