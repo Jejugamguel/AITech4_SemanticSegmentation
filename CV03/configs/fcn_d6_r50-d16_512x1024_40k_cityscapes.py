916922124375dff39f8fe6e3098e2946509f858f
root <wlssud132@gmail.com>
2022-12-24 08:44:43 +0000

    test

--- CV03/configs/fcn_d6_r50-d16_512x1024_40k_cityscapes.py
+++ CV03/configs/fcn_d6_r50-d16_512x1024_40k_cityscapes.py
@@ -8,3 +8,7 @@ model = dict(
     backbone=dict(dilations=(1, 1, 1, 2), strides=(1, 2, 2, 1)),
     decode_head=dict(dilation=6,num_classes=11),
     auxiliary_head=dict(dilation=6, num_classes=11))
+
+runner = dict(type='EpochBasedRunner', max_epochs=50)
+checkpoint_config = dict(by_epoch=True, interval=5)
+evaluation = dict(interval=10, metric='mIoU', pre_eval=True)
