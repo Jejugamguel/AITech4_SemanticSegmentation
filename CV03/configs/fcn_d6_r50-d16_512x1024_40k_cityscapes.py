87b4be114d589f401aea02692139317958e4d1f3
root <wlssud132@gmail.com>
2022-12-26 13:48:24 +0000

    test

--- CV03/configs/fcn_d6_r50-d16_512x1024_40k_cityscapes.py
+++ CV03/configs/fcn_d6_r50-d16_512x1024_40k_cityscapes.py
@@ -3,7 +3,7 @@ _base_ = [
     '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/mmsegmentation/configs/_base_/models/fcn_r50-d8.py',
     '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/CV03/_base_/custom.py',
     '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/CV03/default_runtime.py', 
-    '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/mmsegmentation/configs/_base_/schedules/schedule_40k.py'
+    # '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/mmsegmentation/configs/_base_/schedules/schedule_40k.py'
 ]
 model = dict(
     backbone=dict(dilations=(1, 1, 1, 2), strides=(1, 2, 2, 1)),
@@ -30,7 +30,7 @@ log_config = dict(
             init_kwargs=dict(
                 project='Segmentation_project',
                 entity = 'aitech4_cv3',
-                name = "test_wandb"),)
+                name = "test_JN"),)
         # log_checkpoint=True,
         # log_checkpoint_metadata=True,
         # dict(type='TensorboardLoggerHook')
