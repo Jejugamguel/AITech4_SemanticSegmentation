e417bce3660deaa94663ae09ec944406ee931eb1
Hae <ajhkim1208@naver.com>
2022-12-26 17:55:51 +0000

    base Aug

--- CV03/configs/deeplabv3/deeplabv3_r50-d8_512x512_4x4_20k_coco-stuff10k.py
+++ CV03/configs/deeplabv3/deeplabv3_r50-d8_512x512_4x4_20k_coco-stuff10k.py
@@ -1,8 +1,8 @@
 _base_ = [
-    '../../mmsegmentation/configs/_base_/models/deeplabv3_r50-d8.py',
-    '../_base_/custom.py', 
-    '../../mmsegmentation/configs/_base_/default_runtime.py',
-    '../../mmsegmentation/configs/_base_/schedules/schedule_20k.py'
+    '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/mmsegmentation/configs/_base_/models/deeplabv3_r50-d8.py',
+    '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/CV03/configs/Augmentation/Aug_base.py', 
+    '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/mmsegmentation/configs/_base_/default_runtime.py',
+    '/opt/ml/level2_semanticsegmentation_cv-level2-cv-03/CV03/_base_/scheduler_epochs_60.py'
 ]
 model = dict(
     decode_head=dict(num_classes=11), 
